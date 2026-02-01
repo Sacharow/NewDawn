0a1e21b5faf164c3b5fbf948f840aabfa9e51a44
BartoszStromski <b.stromski.153@studms.ug.edu.pl>
2026-02-01 12:09:05 +0100

    Project Init

---
+++ Frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-red-700">
+    <h1 class="text-4xl font-bold mb-4">Welcome to the RPG App</h1>
+    <p class="text-lg text-gray-700">
+      This is a starter template using Vue 3, Vite, TypeScript, and Tailwind CSS.
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
